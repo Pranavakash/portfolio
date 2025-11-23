b8f8838124cff05215c5f4b02125035ee3574a00
Pranavakash <pranavakash250@gmail.com>
2025-11-23 13:56:28 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,17 +5,17 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <div className="social-icons">
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://www.linkedin.com/in/pranavakash-d?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer" className="social-icon">
             <Linkedin size={24} />
           </a>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+          <a href="https://github.com/Pranavakash" target="_blank" rel="noopener noreferrer" className="social-icon">
             <Github size={24} />
           </a>
           <a href="mailto:pranavakash250@gmail.com" className="social-icon">
             <Mail size={24} />
           </a>
         </div>
-        <p className="copyright">© Pranavakash. made with React.</p>
+        <p className="copyright">© Pranavakash. made with React❤️.</p>
       </div>
     </footer>
   );
